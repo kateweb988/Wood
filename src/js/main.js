dac7190d1955072903473e1d704eb88e61ea2465
kateweb988 <katyavov2805@gmail.com>
2024-07-16 23:18:02 +0300

    fin

---
+++ src/js/main.js
@@ -0,0 +1,319 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  class ItcTabs {
+    constructor(target, config) {
+      const defaultConfig = {};
+      this._config = Object.assign(defaultConfig, config);
+      this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+      this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
+      this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
+      this._eventShow = new Event('tab.itc.change');
+      this._init();
+      this._events();
+    }
+    _init() {
+      this._elTabs.setAttribute('role', 'tablist');
+      this._elButtons.forEach((el, index) => {
+        el.dataset.index = index;
+        el.setAttribute('role', 'tab');
+        this._elPanes[index].setAttribute('role', 'tabpanel');
+      });
+    }
+    show(elLinkTarget) {
+      const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+      const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
+      const elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
+      if (elLinkTarget === elLinkActive) {
+        return;
+      }
+      elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
+      elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
+      elLinkTarget.classList.add('tabs__btn_active');
+      elPaneTarget.classList.add('tabs__pane_show');
+      this._elTabs.dispatchEvent(this._eventShow);
+      elLinkTarget.focus();
+    }
+    showByIndex(index) {
+      const elLinkTarget = this._elButtons[index];
+      elLinkTarget ? this.show(elLinkTarget) : null;
+    };
+    _events() {
+      this._elTabs.addEventListener('click', (e) => {
+        const target = e.target.closest('.tabs__btn');
+        if (target) {
+          e.preventDefault();
+          this.show(target);
+        }
+      });
+    }
+  }
+
+  // инициализация .tabs как табов
+  new ItcTabs('.tabs');
+});
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+          return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+  var accordeonButtons = document.getElementsByClassName("accordeon__button");
+
+  //пишем событие при клике на кнопки - вызов функции toggle
+  for (var i = 0; i < accordeonButtons.length; i++) {
+    var accordeonButton = accordeonButtons[i];
+
+    accordeonButton.addEventListener("click", toggleItems, false);
+  }
+
+  //пишем функцию
+  function toggleItems() {
+
+    // переменная кнопки(актульная) с классом
+    var itemClass = this.className;
+
+    // добавляем всем кнопкам класс close
+    for (var i = 0; i < accordeonButtons.length; i++) {
+      accordeonButtons[i].className = "accordeon__button closed";
+    }
+
+    // закрываем все открытые панели с текстом
+    var pannels = document.getElementsByClassName("accordeon__panel");
+    for (var z = 0; z < pannels.length; z++) {
+      pannels[z].style.maxHeight = 0;
+    }
+
+    // проверка. если кнопка имеет класс close при нажатии
+    // к актуальной(нажатой) кнопке добававляем активный класс
+    // а панели - которая находится рядом задаем высоту
+    if (itemClass == "accordeon__button closed") {
+      this.className = "accordeon__button active";
+      var panel = this.nextElementSibling;
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+  }
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper1 = new Swiper('.swiper1', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination1",
+    },
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 2,
+    spaceBetween: 24,
+    pagination: {
+      el: ".swiper-pagination2",
+    },
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 10,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        slidesPerView: 2
+      },
+      1200: {
+        spaceBetween: 24,
+        slidesPerView: 2
+      }
+    }
+  });
+  const swiper3 = new Swiper('.swiper3', {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    pagination: {
+      el: ".swiper-pagination3",
+    },
+    navigation: {
+      nextEl: '.swiper-button-next3',
+      prevEl: '.swiper-button-prev3',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 10,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        slidesPerView: 2
+      },
+      1200: {
+        spaceBetween: 24,
+        slidesPerView: 3
+      }
+    }
+  });
+  var swiper5 = new Swiper(".swiper5", {
+    spaceBetween: 24,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper4 = new Swiper(".swiper4", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next4",
+      prevEl: ".swiper-button-prev4",
+    },
+    pagination: {
+      el: ".swiper-pagination4",
+    },
+    thumbs: {
+      swiper: swiper5,
+    },
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $(document).ready(function () {
+    $(".youtube-link").grtyoutube({
+      autoPlay: true
+    });
+  });
+
+  (function ($) {
+
+    $.fn.grtyoutube = function (options) {
+
+      return this.each(function () {
+
+        // Get video ID
+        var getvideoid = $(this).attr("youtubeid");
+
+        // Default options
+        var settings = $.extend({
+          videoID: getvideoid,
+          autoPlay: true
+        }, options);
+
+        // Convert some values
+        if (settings.autoPlay === true) { settings.autoPlay = 1 } else { settings.autoPlay = 0 }
+
+        // Initialize on click
+        if (getvideoid) {
+          $(this).on("click", function () {
+            $("body").append('<div class="grtvideo-popup">' +
+              '<div class="grtvideo-popup-content">' +
+              '<span class="grtvideo-popup-close">&times;</span>' +
+              '<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/' + settings.videoID + '?rel=0&wmode=transparent&autoplay=' + settings.autoPlay + '&iv_load_policy=3" allowfullscreen frameborder="0"></iframe>' +
+              '</div>' +
+              '</div>');
+          });
+        }
+
+        // Close the box on click or escape
+        $(this).on('click', function (event) {
+          event.preventDefault();
+          $(".grtvideo-popup-close, .grtvideo-popup").click(function () {
+            $(".grtvideo-popup").remove();
+          });
+        });
+
+        $(document).keyup(function (event) {
+          if (event.keyCode == 27) {
+            $(".grtvideo-popup").remove();
+          }
+        });
+      });
+    };
+  }(jQuery));
+});
+document.addEventListener("DOMContentLoaded", () => {
+  let menuBtn = document.querySelector('.menu-btn');
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
+// svg
+$(function () {
+  jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, else we gonna set it if we can.
+      if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
+
+  });
+});
